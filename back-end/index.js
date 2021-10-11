ce583cd4e9895a2576f4efa6ea727a63f353ec63
Danny <dy31003@gmail.com>
2021-10-10 20:48:54 -0400

    added procfile

--- back-end/index.js
+++ back-end/index.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const app = express()
 const database = require('./queries')
-const PORT = 3000 || process.env.PORT // defaults to localhost -> 3000
+const PORT = process.env.PORT || 3000 // defaults to localhost -> 3000
 app.use(express.json())
 app.use(
     express.urlencoded({
