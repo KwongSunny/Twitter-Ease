3984931638156a777a6f42118ddd584adda9cf81
Danny <dy31003@gmail.com>
2021-10-10 22:24:54 -0400

    test

---
+++ back-end/config.js
@@ -0,0 +1,19 @@
+//connecting to Potsgres database from Node
+require('dotenv').config()          // dot config
+const Pool = require('pg').Pool
+/*const pool = new Pool({
+    user: 'me',
+    host: 'localhost',
+    database: 'api',
+    password: 'SQL8103!',
+    port: 5432
+})
+*/
+// set environment to production to ensure minimal logging and optimize performance
+const production = process.env.NODE_ENV === 'production'
+const connection = `postgresql://${process.env.DB_USER}:${process.env.DB_PW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+const pool = new Pool({
+    connectionString:production ? process.env.DATABASE_URL:connection,ssl:production
+})
+
+module.exports = pool
--- back-end/queries.js
+++ back-end/queries.js
@@ -1,21 +1,4 @@
-//connecting to Potsgres database from Node
-require('dotenv').config()          // dot config
-const Pool = require('pg').Pool
-
-/*const pool = new Pool({
-    user: 'me',
-    host: 'localhost',
-    database: 'api',
-    password: 'SQL8103!',
-    port: 5432
-})
-*/
-// set environment to production to ensure minimal logging and optimize performance
-const production = process.env.NODE_ENV === 'production'
-const connection = `postgresql://${process.env.DB_USER}:${process.env.DB_PW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-const pool = new Pool({
-    connectionString:production ? process.env.DATABASE_URL:connection,ssl:production
-})
+const pool = require('./config')
 // get all users
 const getUsers = (req,res) => {
     pool.query('SELECT * FROM accounts ORDER BY id', (error,result) => {
