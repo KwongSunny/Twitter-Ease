a466c09fa0b996373a6b5eb7642e24dc3b239104
Danny <dy31003@gmail.com>
2021-10-10 22:38:39 -0400

    commit

--- back-end/config.js
+++ back-end/config.js
@@ -13,7 +13,7 @@ const Pool = require('pg').Pool
 const production = process.env.NODE_ENV === 'production'
 const connection = `postgresql://${process.env.DB_USER}:${process.env.DB_PW}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 const pool = new Pool({
-    connectionString:production ? process.env.DATABASE_URL:connection,ssl:production
+    connectionString:production ? process.env.DATABASE_URL:connection
 })
 
 module.exports = pool
