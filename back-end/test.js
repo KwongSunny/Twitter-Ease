e32266e34e2c85410eb6d8554f75d5d0e95b752b
christy8059 <christyl8059@hstat.org>
2021-10-11 21:20:36 -0400

    added 3 features

--- back-end/test.js
+++ back-end/test.js
@@ -1,7 +1,24 @@
 require("dotenv").config();
 const twit = require('./twitter');
 
-// just test
-twit.twitterAPI.post('statuses/update', {status:'hello world!'  },function(err,data,response) {
-    console.log(data)
-})
+
+//-----------------SEND DIRECT MESSAGE
+
+function sendDM(userID,message){
+
+
+    let userName = {recipient_id:userID, message_data:message};
+
+    twit.twitterAPI.post("direct_messages/events/new", userName, (err, data) => {
+        if(err){
+            console.log("Error sending DM"); 
+            return; 
+        }
+        if (data){
+            console.log("DM sent"); 
+        }
+
+
+    }
+
+sendDM(christy_o_o, "hi"); 
