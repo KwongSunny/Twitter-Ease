75450053f5c02934791fb334e7f683b303a3b61c
Leonardo Ridwan <leonardoridwan@yahoo.com>
2021-10-11 23:12:30 -0400

    turned hexadecimal to RGB

--- front-end/src/utils/LoginBoxUtils.js
+++ front-end/src/utils/LoginBoxUtils.js
@@ -1,18 +1,55 @@
-function highlightButton(element){
-    let isHover = (e) => e.parentElement.querySelector(':hover') === e;
-    document.addEventListener('mousemove', function checkHover() {
-        const hovered = isHover(element);
-        if (hovered !== checkHover.hovered) {
-            console.log(hovered ? 'hovered' : 'not hovered');
-            checkHover.hovered = hovered;
-        }
-    })
+// function highlightButton(element){
+//     let isHover = (e) => e.parentElement.querySelector(':hover') === e;
+//     document.addEventListener('mousemove', function checkHover() {
+//         const hovered = isHover(element);
+//         if (hovered !== checkHover.hovered) {
+//             console.log(hovered ? 'hovered' : 'not hovered');
+//             checkHover.hovered = hovered;
+//         }
+//     })
 
-}
-export default highlightButton;
+// }
+// //export default highlightButton;
 
 //checks the string password for a capital letter and a symbol
 //returns a boolean
 function checkValidPassword(password){
-    
+    if(password.length == 0){
+        console.log("Password cannot be empty");
+        return false;
+    }
+    else if(password.length < 8){
+        console.log("Password must contain at least 8 characters");
+        return false;
+    }
+    re = /[0-9]/;
+    if(!re.test(password)){
+        console.log("Password must contain at least 1 number from (0-9)");
+        return false;
+    }
+    re = /[a-z]/;
+    if(!re.test(password)){
+        console.log("Password must contain at least 1 lower case letter from (a-z)");
+        return false;
+    }
+    re = /[A-Z]/;
+    if(!re.test(password)){
+        console.log("Password must contain at least 1 upper case letter from (A-Z)");
+        return false;
+    }
+    re = /[!@#$%^&*]/;
+    if(!re.test(password)){
+        console.log("Password must contain at least 1 special character from (!@#$%^&*)");
+        return false;
+    }
+    console.log("You have entered a valid password");
+        return true;
+
+    //return password === /^(?=.[0-9])(?=.[!@#$%^&])[a-zA-Z0-9!@#$%^&]{8,}$/; // using regex to check if theres at least one upper case letter, symbol, and number
 }
+  console.log(checkValidPassword('nwvv!#@QDS123'))
+  console.log(checkValidPassword('12345536547658'))
+  console.log(checkValidPassword('Aa12!'))
+  console.log(checkValidPassword('VVVVVVVVVVV'))
+  console.log(checkValidPassword('Testcode123!'))
+  console.log(checkValidPassword(''))
--- front-end/src/utils/Utils.js
+++ front-end/src/utils/Utils.js
@@ -3,10 +3,9 @@
 //returns a JSON of separated red, green, blue values in base 10
 function hexStringToJson(hexString){
     let string = hexString.substring(1);
-    
-    let r_val = null;
-    let g_val = null;
-    let b_val = null;
+    let r_val = parseInt(string.substring(0,2), 16);
+    let g_val = parseInt(string.substring(2,4), 16);
+    let b_val = parseInt(string.substring(4,6), 16);
 
     return {
         red: r_val,
@@ -14,4 +13,3 @@ function hexStringToJson(hexString){
         blue: b_val
     };
 }
-console.log(hexStringToJson("#012345"));
