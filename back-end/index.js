6593a9828567194b6f82d8e149cdf1306012b05e
Danny <dy31003@gmail.com>
2021-10-11 22:58:29 -0400

    database backend

---
+++ back-end/index.js
@@ -0,0 +1,29 @@
+//set up server using express
+const express = require('express')
+const PORT = process.env.PORT || 3002 // defaults to localhost -> 3000
+const production = process.env.NODE_ENV === 'production'
+const origin = {
+  origin: production ? 'https://twitter-ease-api.herokuapp.com/' : '*'
+}
+app.use(cors(origin))
+
+const app = express()
+const database = require('./queries')
+app.use(express.json())
+app.use(
+    express.urlencoded({
+    extended: true
+  })
+)
+app.get('/', (req, res) => {
+    res.json({ info: 'Postgres API' })
+})
+app.get('/accounts', database.getUsers)
+app.get('/accounts/:id', database.getUserById)
+app.post('/accounts', database.createUser)
+app.put('/accounts/:id', database.updateUser)
+app.delete('/accounts/:id', database.deleteUser)
+
+app.listen(PORT,() => {
+    console.log(`Running on port ${PORT}.`)
+})    
---
+++ back-end/queries.js
@@ -0,0 +1,78 @@
+const pool = require('./config')
+// get all users
+const getUsers = (req,res) => {
+    if(!req.header('apiKey') || req.header('apiKey') !== process.env.API_KEY) {
+        return res.status(401).json({status: 'error',message: 'Unauthorized'})
+    }
+    pool.query('SELECT * FROM accounts ORDER BY id', (error,result) => {
+        if(error){
+            throw error
+        }
+        res.status(200).json(result.rows)
+    })
+}
+// get user by id
+const getUserById = (req,res) => {
+    if(!req.header('apiKey') || req.header('apiKey') !== process.env.API_KEY) {
+        return res.status(401).json({status: 'error',message: 'Unauthorized'})
+    }
+    const id = req.params.id
+    console.log(id)
+    pool.query('SELECT * FROM accounts WHERE id = $1',[id],(error,result) => {
+        if(error) {
+            throw error
+        }
+        res.status(200).json(result.rows)
+    })
+}
+// post new user 
+const createUser = (req,res) => {
+    if(!req.header('apiKey') || req.header('apiKey') !== process.env.API_KEY) {
+        return res.status(401).json({status: 'error',message: 'Unauthorized'})
+    }
+    const{id,email,password} = req.body
+    pool.query('INSERT INTO accounts (id,email,password) VALUES ($1,$2,$3)',[id,email,password],(error,result)=>{
+        if(error){
+            throw error
+        }
+        res.status(201).send(`Created ID: ${id}`)
+    })
+}
+// update user
+const updateUser = (req,res) => {
+    if(!req.header('apiKey') || req.header('apiKey') !== process.env.API_KEY) {
+        return res.status(401).json({status: 'error',message: 'Unauthorized'})
+    }
+    const id = req.params.id
+    const {password} = req.body
+    pool.query(
+        'UPDATE accounts SET password = $1',[password],(error,result) => {
+            if(error) {
+                throw error
+            }
+            res.status(200).send(`User modified with ID: ${id}`)
+        }
+    )
+}
+// delete user
+const deleteUser = (req,res) => {
+    if(!req.header('apiKey') || req.header('apiKey') !== process.env.API_KEY) {
+        return res.status(401).json({status: 'error',message: 'Unauthorized'})
+    }
+    const id = req.params.id
+    pool.query('DELETE FROM accounts WHERE id = $1',[id],(error,result)=>{
+        if(error){  
+            throw error
+        }
+        res.status(200).send(`User id ${id} deleted`) 
+    })
+}
+// export modules 
+module.exports = {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+  }
+  
