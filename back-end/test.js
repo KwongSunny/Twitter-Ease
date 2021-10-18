c53ad9b83e4a4d2f0837bd8176cec4b14757bedd
Danny <dy31003@gmail.com>
2021-10-18 18:47:25 -0400

    fixed?

--- back-end/test.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const twit = require('./twitter');
-
-// just test
-twit.twitterAPI.post('statuses/update', {status:'hello world!'},function(err,data,response) {
-    console.log(data)
-})
-
