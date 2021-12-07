72121a2413c1c135f76de97688919135d59af312
Danny <dy31003@gmail.com>
2021-12-07 02:27:46 -0500

    heroku

--- oauth_index.js
+++ oauth_index.js
@@ -25,21 +25,16 @@ const COOKIE_SECRET = process.env.npm_config_cookie_secret || process.env.COOKIE
   app.use(cookieParser())
   app.use(session({ secret: COOKIE_SECRET || 'secret' }))
   //static file
-  app.use(express.static(path.join(__dirname, 'client/build')));
-  //production
+  app.listen(PORT, () => console.log(`listening on ${PORT}`))
+
+
+  // ************ROUTERS AND STUFF**********
   if(process.env.NODE_ENV === 'production') { 
     app.use(express.static(path.join(__dirname, 'client/build')));
     app.get('*', (req, res) => {
        res.sendFile(path.join(__dirname = 'client/build/index.html'));  
       })
   }
-  
-  app.get('*', (req, res) => {  res.sendFile(path.join(__dirname + '/client/public/index.html'));})
-  app.listen(PORT, () => console.log(`listening on ${PORT}`))
-
-
-  // ************ROUTERS AND STUFF**********
-
   // simple home page
   app.get('/', (req, res, next) => {
     res.redirect('https://twitter-ease.herokuapp.com/')
